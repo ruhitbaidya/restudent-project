302c5d50d176ac2d10e2bf3d2ad50d3d32962a70
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-05-18 19:00:55 +0600

    some text find

--- bristo-boss/src/Pages/ShopNow.jsx
+++ bristo-boss/src/Pages/ShopNow.jsx
@@ -4,18 +4,21 @@ import SheafServices from '../Layout/sheafServices';
 import banner2 from "../assets/shop/banner2.jpg"
 import useDataget from '../Layout/shared/useDataget';
 import OurShop from '../Layout/shared/OurShop';
+import { useState } from 'react';
 const ShopNow = () => {
+  const [tabIndex, setTabIndex] = useState(0);
     const [menu] = useDataget();
     const offered = menu.filter((item)=> item.category === "offered");
     const dessert = menu.filter((item)=> item.category === "dessert");
     const pizza = menu.filter((item)=> item.category === "pizza");
     const salad = menu.filter((item)=> item.category === "salad");
     const soup = menu.filter((item)=> item.category === "soup");
+    console.log(tabIndex)
   return (
     <>
 <SheafServices textcolor="200" image={banner2} title="OUR SHOP" desc="Would you like to try a dish?" colorsfind="rgba(36, 34, 34, 0.623)"></SheafServices>
 
-<Tabs direction="center">
+<Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
     <TabList>
       <Tab>Salad</Tab>
       <Tab>pizza</Tab>
